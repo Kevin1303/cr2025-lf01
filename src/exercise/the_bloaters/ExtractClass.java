ce6126f9d37c6464a60e203b30a530b5dd452ceb
Kevin1303 <161975109+Kevin1303@users.noreply.github.com>
2025-02-15 15:58:14 +0700

    ExtractClass_Selesai

--- src/exercise/the_bloaters/ExtractClass.java
+++ src/exercise/the_bloaters/ExtractClass.java
@@ -3,26 +3,32 @@ public class ExtractClass {
 // todo extract class TelephoneNumber from class Person
 	class Person {
 	  private String name;
-	  private String officeAreaCode;
-	  private String officeNumber;
-
+	  private TelephoneNumber PhoneNumber;
 	  public String getName() {
 		return name;
 	  }
 	  public String getTelephoneNumber() {
-		return ("(" + officeAreaCode + ") " + officeNumber);
+		return (PhoneNumber.getTelephoneNumber());
 	  }
-	  public String getOfficeAreaCode() {
-		return officeAreaCode;
-	  }
-	  public void setOfficeAreaCode(String arg) {
-		officeAreaCode = arg;
-	  }
-	  public String getOfficeNumber() {
-		return officeNumber;
-	  }
-	  public void setOfficeNumber(String arg) {
-		officeNumber = arg;
-	  }
+	}
+	class TelephoneNumber{
+		private String officeAreaCode;
+		private String officeNumber;
+		
+		 public String getTelephoneNumber() {
+				return ("(" + officeAreaCode + ") " + officeNumber);
+			  }
+			  public String getOfficeAreaCode() {
+				return officeAreaCode;
+			  }
+			  public void setOfficeAreaCode(String arg) {
+				officeAreaCode = arg;
+			  }
+			  public String getOfficeNumber() {
+				return officeNumber;
+			  }
+			  public void setOfficeNumber(String arg) {
+				officeNumber = arg;
+			  }
 	}
 }
